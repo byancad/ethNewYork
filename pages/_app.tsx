4b1fb22eaeae577e60aae67b200df3a0a5672f4a
Byanca De la fuente <byancadlf@gmail.com>
2022-06-24 22:02:19 -0500

    add wagmi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'contexts/userContext'
+import { Wagmi } from 'components/Wagmi/Wagmi'
+import { WagmiConfig } from 'wagmi'
+import { client } from 'configs/wagmi'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (  
   <Provider>
+    <WagmiConfig client={client}>
+    <Wagmi/>
     <Component {...pageProps} />
+    </WagmiConfig>
+    
  </Provider>
  )
 
