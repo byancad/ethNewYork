3dc98af31cd2448ec64639dd7e9ae22a29e77743
Byanca De la fuente <byancadlf@gmail.com>
2022-06-25 22:01:11 -0500

    add landing page

---
+++ components/Notices/WrongChain.tsx
@@ -0,0 +1,8 @@
+export const WrongChain = () => {
+  return (
+    <div>
+      You are connected to the wrong chain! Switch to one of our supported
+      networks to continue
+    </div>
+  );
+};
